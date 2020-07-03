36157388c529deafb7aa5d883a706ecfed669656
MWGuy <yousan4ik@gmail.com>
2020-07-04 02:08:51 +0300

    Build to DevelNext buindle support

---
+++ src-bundle/develnext/bundle/jediterm/JeditermBundle.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace develnext\bundle\jediterm;
+
+use ide\bundle\AbstractBundle;
+use ide\bundle\AbstractJarBundle;
+use ide\project\Project;
+use ide\library\IdeLibraryBundleResource;
+
+/**
+ * Class OSHIBundle
+ * @package develnext\bundle\jediterm
+ */
+class JeditermBundle extends AbstractJarBundle
+{
+    /**
+     * @param Project $project
+     * @param AbstractBundle|null $owner
+     */
+    public function onAdd(Project $project, AbstractBundle $owner = null){
+        parent::onAdd($project, $owner);
+    }
+
+    /**
+     * @param Project $project
+     * @param AbstractBundle|null $owner
+     */
+    public function onRemove(Project $project, AbstractBundle $owner = null){
+        parent::onRemove($project, $owner);
+    }
+
+    /**
+     * @param IdeLibraryBundleResource $resource
+     */
+    public function onRegister(IdeLibraryBundleResource $resource)
+    {
+        parent::onRegister($resource);
+    }
+}
